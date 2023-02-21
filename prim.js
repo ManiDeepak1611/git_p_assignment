1ee530015106366ca3b5ca3cfda9469486241373
C H A N D U <manideepak1611@gmail.com>
2023-02-21 19:38:53 +0530

    correction done and changed test case

--- prim.js
+++ prim.js
@@ -1,14 +1,14 @@
 // Code to check if a given number is prime or not.
 
-let num=7;
+let num=10;
 let count=0;
 for(let i=1;i<=num;i++){
 	if(num%i===0){
 	count++;
 	}
 }
-If(count==2){
+if(count==2){
 console.log("Prime");
 }else{
-console.loge("Not Prime");
+console.log("Not Prime");
 }
